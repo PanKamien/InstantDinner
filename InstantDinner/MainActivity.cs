71fef0f4eed1f19e251fa14c2a70f052c103eceb
PanKamien <dzik.adrian@gmail.com>
2017-03-08 13:16:35 +0100

    Theme.DeviceDefault.Light.NoActionBar

--- InstantDinner/MainActivity.cs
+++ InstantDinner/MainActivity.cs
@@ -4,14 +4,12 @@ using Android.OS;
 
 namespace InstantDinner
 {
-    [Activity(Label = "InstantDinner", MainLauncher = true, Icon = "@drawable/icon")]
+    [Activity(Label = "InstantDinner", MainLauncher = true, Icon = "@drawable/icon", Theme = "@android:style/Theme.DeviceDefault.Light.NoActionBar")]
     public class MainActivity : Activity
     {
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
-            
-
             SetContentView (Resource.Layout.Main);
         }
     }
