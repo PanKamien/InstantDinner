7fd58939937a77f39651026e9118e131b6b00230
PanKamien <dzik.adrian@gmail.com>
2017-03-24 23:59:31 +0100

    Added input ingredients support

--- InstantDinner/AddProducts.cs
+++ InstantDinner/AddProducts.cs
@@ -19,7 +19,12 @@ namespace InstantDinner
     public class AddProducts : Activity
     {
         TextView txtViewRecipeTitle, txtViewRecipeCount;
+        EditText edtTxtProduct1, edtTxtProduct2, edtTxtProduct3, edtTxtProduct4, edtTxtProduct5;
         int i, recipeCount;
+        string key;
+        Button button1;
+
+
         public override void OnBackPressed()
         {
             i = 0;
@@ -29,61 +34,77 @@ namespace InstantDinner
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
+            key = "9393b6d777ae25211c8b14a569882e64";
+
 
 
 
             // Create your application here
             SetContentView(Resource.Layout.AddProducts);
             txtViewRecipeTitle = FindViewById<TextView>(Resource.Id.txtViewRecipeTitle);
-            var button1 = FindViewById<Button>(Resource.Id.button1);
+            button1 = FindViewById<Button>(Resource.Id.button1);
             button1.Enabled = true;
             i = 0;
-            SearchRecipe();
-
-
-
-
-            txtViewRecipeCount = FindViewById<TextView>(Resource.Id.txtViewRecipeCount);
             
 
 
 
 
+            txtViewRecipeCount = FindViewById<TextView>(Resource.Id.txtViewRecipeCount);
+            edtTxtProduct1 = FindViewById<EditText>(Resource.Id.edtTxtProduct1);
+            edtTxtProduct2 = FindViewById<EditText>(Resource.Id.edtTxtProduct2);
+            edtTxtProduct3 = FindViewById<EditText>(Resource.Id.edtTxtProduct3);
+            edtTxtProduct4 = FindViewById<EditText>(Resource.Id.edtTxtProduct4);
+            edtTxtProduct5 = FindViewById<EditText>(Resource.Id.edtTxtProduct5);
+
+            recipeCount = 5;
+
+
             button1.Click += (s, e) =>
             {
                 
-                if(i < recipeCount -1 )
+                if(i < recipeCount )
                 {
                     SearchRecipe();
-                    i++;
+                    
                 }
                 else
                 {
                     button1.Enabled = false;
                     txtViewRecipeTitle.Text = "No more recipes";
                 }
-                
 
+                
             };
 
             
         }
 
-
+        //shredded%20chicken,salt,bread
 
         public async void SearchRecipe()
         {
             RootObject dane;
 
+            string ingredient1 = edtTxtProduct1.Text;
+            string ingredient2 = edtTxtProduct2.Text;
+            string ingredient3 = edtTxtProduct3.Text;
+            string ingredient4 = edtTxtProduct4.Text;
+            string ingredient5 = edtTxtProduct5.Text;
+
+            button1.Text = "Next";
+
+            
+
             using (var httpClient = new HttpClient())
             {
-                string url = "http://food2fork.com/api/search?key=9393b6d777ae25211c8b14a569882e64&q=shredded%20chicken,salt,bread";
+                string url = String.Format("http://food2fork.com/api/search?key={0}&q={1},{2},{3},{4},{5}", key, ingredient1, ingredient2, ingredient3, ingredient4, ingredient5);
                 var json = await httpClient.GetStringAsync(url);
                 dane = JsonConvert.DeserializeObject<RootObject>(json);
                 recipeCount = dane.count;
                 txtViewRecipeCount.Text = "Number of recipes: " + recipeCount;
                 txtViewRecipeTitle.Text = (i+1) +  ". " + dane.recipes[i].title;
-
+                i++;
 
             }
             
